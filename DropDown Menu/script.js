a979851c9f908155750786d743445011f3e271ea
mathnerdrk <rahul.krishnan@hotmail.com>
2016-08-28 17:16:17 -0600

    Added Animation and Item Hover

--- DropDown Menu/script.js
+++ DropDown Menu/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $(".dropt-btn").hover(function() {
     $("#idDropDown").removeClass("dropdown-content");
+    $("#idDropDown").hide().slideDown();
+  },
+  function() {
+    $("#idDropDown").addClass("dropdown-content");
+  });
+
+  $("#idDropDown").hover(function() {
+    $("#idDropDown").removeClass("dropdown-content");
   },
   function() {
     $("#idDropDown").addClass("dropdown-content");
