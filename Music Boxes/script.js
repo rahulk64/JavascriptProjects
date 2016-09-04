f7b8243a8f40aa032892b855552f2154dc98b535
Rahul Krishnan <rahul.krishnan@hotmail.com>
2016-09-04 12:57:26 -0600

    Allow for Key Presses

--- Music Boxes/script.js
+++ Music Boxes/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //Audio File Imports
   var cNote = document.getElementById("cAudio");
   var dNote = document.getElementById("dAudio");
   var eNote = document.getElementById("eAudio");
@@ -7,6 +8,7 @@ $(document).ready(function() {
   var aNote = document.getElementById("aAudio");
   var bNote = document.getElementById("bAudio");
 
+  //Play Audio through Clicking
   $("#c").mousedown(function(){
     cNote.currentTime = 0;
     cNote.play();
@@ -41,4 +43,85 @@ $(document).ready(function() {
     bNote.currentTime = 0;
     bNote.play();
   });
+
+  //Play Audio through Key Presses
+  //Key Down: Play Audio, Turn on Active
+  $(window).keydown(function(e) {
+     switch(e.which) {
+       case 49:
+         cNote.currentTime = 0;
+         cNote.play();
+         $("#c").addClass("activate");
+         break;
+
+       case 50:
+         dNote.currentTime = 0;
+         dNote.play();
+         $("#d").addClass("activate");
+         break;
+
+       case 51:
+         eNote.currentTime = 0;
+         eNote.play();
+         $("#e").addClass("activate");
+         break;
+
+       case 52:
+         fNote.currentTime = 0;
+         fNote.play();
+         $("#f").addClass("activate");
+         break;
+
+       case 53:
+         gNote.currentTime = 0;
+         gNote.play();
+         $("#g").addClass("activate");
+         break;
+
+       case 54:
+         aNote.currentTime = 0;
+         aNote.play();
+         $("#a").addClass("activate");
+         break;
+
+       case 55:
+         bNote.currentTime = 0;
+         bNote.play();
+         $("#b").addClass("activate");
+         break;
+     }
+   });
+
+   //Turn off Active
+   $(window).keyup(function(e) {
+     switch(e.which) {
+       case 49:
+         $("#c").removeClass("activate");
+         break;
+
+       case 50:
+         $("#d").removeClass("activate");
+         break;
+
+       case 51:
+         $("#e").removeClass("activate");
+         break;
+
+       case 52:
+         $("#f").removeClass("activate");
+         break;
+
+       case 53:
+         $("#g").removeClass("activate");
+         break;
+
+       case 54:
+         $("#a").removeClass("activate");
+         break;
+
+       case 55:
+         $("#b").removeClass("activate");
+         break;
+     }
+   });
 });
